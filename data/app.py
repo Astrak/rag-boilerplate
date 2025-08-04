4336a4969f9f953f1a86ccb25477bbf4fcdd630e
Astrak <Astrak@users.noreply.github.com>
2025-08-04 18:50:03 +0000

    Fix missing pkg

--- data/app.py
+++ data/app.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-from langchain.vectorstores import FAISS
+from langchain_community.vectorstores import FAISS
 from langchain_openai import OpenAIEmbeddings
 import boto3
 import os
