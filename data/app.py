9597138f5f8c7eb8c2f84d9a2b4bcb1f0dd50946
Astrak <Astrak@users.noreply.github.com>
2025-08-04 19:43:33 +0000

    Test full RAG logic

--- data/app.py
+++ data/app.py
@@ -4,6 +4,19 @@ from langchain_openai import OpenAIEmbeddings
 import boto3
 import os
 from pydantic import BaseModel
+from langchain_core.prompts import PromptTemplate
+from langchain_core.documents import Document
+from typing_extensions import List, TypedDict
+from langchain.chat_models import init_chat_model
+from langgraph.graph import START, StateGraph
+
+prompt = PromptTemplate.from_template("You are an assistant for question-answering tasks. " +
+"Use the following pieces of retrieved context to answer the question." + 
+"If you don't know the answer, just say that you don't know. " +
+"Use three sentences maximum and keep the answer concise. " +
+"""Question: {question} 
+Context: {context} 
+Answer:""")
 
 openai_api_key = os.getenv("OPENAI_API_KEY")
 if not openai_api_key:
@@ -11,10 +24,17 @@ if not openai_api_key:
 
 os.environ["OPENAI_API_KEY"] = openai_api_key
 
-app = FastAPI()
+langsmith_api_key = os.getenv("LANGSMITH_API_KEY")
+if not langsmith_api_key:
+    raise EnvironmentError("LANGSMITH_API_KEY not found")
 
-class SearchRequest(BaseModel):
-    question: str
+os.environ["LANGSMITH_API_KEY"] = langsmith_api_key
+
+google_api_key = os.getenv("GOOGLE_API_KEY")
+if not google_api_key:
+    raise EnvironmentError("GOOGLE_API_KEY not found")
+
+os.environ["GOOGLE_API_KEY"] = google_api_key
 
 os.makedirs("./vectorstore", exist_ok=True)
 s3 = boto3.client('s3', region_name="eu-north-1")
@@ -24,10 +44,34 @@ s3.download_file("rag-faiss-index-bucket", "vectorstores/index.pkl", "./vectorst
 embeddings = OpenAIEmbeddings(model="text-embedding-3-large")
 vector_store = FAISS.load_local("./vectorstore", embeddings, allow_dangerous_deserialization=True)
 
+class State(TypedDict):
+    question: str
+    context: List[Document]
+    answer: str
+
+def retrieve_from_store1(state: State):
+    retrieved_docs = vector_store.similarity_search(state["question"])
+    return {"context": retrieved_docs}
+
+def generate(state: State):
+    docs_content = "\n\n".join(doc.page_content for doc in state["context"])
+    messages = prompt.invoke({"question": state["question"], "context": docs_content})
+    llm = init_chat_model("gemini-2.5-flash", model_provider="google_genai")
+    response = llm.invoke(messages)
+    return {"answer": response.content}
+
+graph_builder = StateGraph(State).add_sequence([retrieve_from_store1, generate])
+graph_builder.add_edge(START, "retrieve_from_store1")
+graph = graph_builder.compile()
+
+app = FastAPI()
+
+class SearchRequest(BaseModel):
+    question: str
+
 @app.post("/search")
 def search(request: SearchRequest):
-    print('search request received: {request.question}')
-    query = request.question
-    docs = vector_store.similarity_search(query)
+    print('search request received: ' + request.question)
+    graph.invoke({"question": "request.question"}) # pyright: ignore[reportArgumentType]
     print('similarity search finished')
     return {"results": [doc.page_content for doc in docs]}
