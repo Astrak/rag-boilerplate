d86ff3c5ad956d9add704c29fc7b0904b4a9b145
Astrak <Astrak@users.noreply.github.com>
2025-08-08 09:30:07 +0000

    Fix rewriting store, reduce processed articles for cost

--- app/vector_store.py
+++ app/vector_store.py
@@ -4,10 +4,10 @@ from langchain_openai import OpenAIEmbeddings
 from langchain_community.vectorstores import FAISS
 
 def get_store():
-    os.makedirs("./vectorstore", exist_ok=True)
-    s3 = boto3.client('s3', region_name="eu-north-1")
-    s3.download_file("rag-faiss-index-bucket", "vectorstores/index.faiss", "./vectorstore/index.faiss")
-    s3.download_file("rag-faiss-index-bucket", "vectorstores/index.pkl", "./vectorstore/index.pkl")
+    # os.makedirs("./vectorstore", exist_ok=True)
+    # s3 = boto3.client('s3', region_name="eu-north-1")
+    # s3.download_file("rag-faiss-index-bucket", "vectorstores/index.faiss", "./vectorstore/index.faiss")
+    # s3.download_file("rag-faiss-index-bucket", "vectorstores/index.pkl", "./vectorstore/index.pkl")
 
     embeddings = OpenAIEmbeddings(model="text-embedding-3-large")
     return FAISS.load_local("./vectorstore", embeddings, allow_dangerous_deserialization=True)
