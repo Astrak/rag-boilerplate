7b91a9e7fe673e3ba1d1378250cf3da2ab58997a
Astrak <coffre.a@gmail.com>
2025-08-09 19:25:33 +0200

    Update prompt

--- app/graph.py
+++ app/graph.py
@@ -25,10 +25,10 @@ class Graph:
         return {"context": retrieved_docs}
 
     def generate(self, state: State):
-        print(state['context'][0])
-        print('#############')
-        print(state['context'][0].metadata)
-        docs_content = "\n\n".join(doc.page_content for doc in state["context"])
+        contents = list[str]
+        for doc in state['context']
+            contents.append(f'{doc.page_content}\nAuteur: {doc.metadata.author}\nDate: {doc.metadata.date}\nSource: {doc.metadata.url}\nTitre: {doc.metadata.title}')
+        docs_content = "\n\n".join(contents)
         messages = self.prompt.invoke({"question": state["question"], "context": docs_content})
         llm = init_chat_model("gemini-2.5-flash", model_provider="google_genai")
         response = llm.invoke(messages)
