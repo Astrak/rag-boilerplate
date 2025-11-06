a33f0ee7890016dfa41df09011a7acefeb4d9e1e
Astrak <coffre.a@gmail.com>
2025-11-06 18:30:17 +0100

    Add articles downloader module

---
+++ src/scraper/download_articles/main.py
@@ -0,0 +1,11 @@
+from src.scraper.download_articles import ArticleDownloader
+
+FOLDER = "./ojim-urls/"
+
+ARTICLE_SELECTOR = "#singleojimcontent" # "#contenu" pour Polémia
+
+# Make sure to check the ArticleDownloader#scrape_article method and vet how metadata, author, date and title are selected.
+
+discoverer = ArticleDownloader(folder=FOLDER, article_selector=ARTICLE_SELECTOR)
+discoverer.sync_urls_from_bucket()
+discoverer.scrape_articles()
