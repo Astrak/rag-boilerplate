22c1ce841ee5b63578ed2a6e339c95bb45edac78
Astrak <coffre.a@gmail.com>
2025-11-07 12:49:33 +0100

    Make variable come from env

--- src/scraper/discover_urls/main.py
+++ src/scraper/discover_urls/main.py
@@ -1,7 +1,11 @@
 from scraper.discover_urls.url_discoverer import UrlDiscoverer
+import os
 
-BASE_URL="https://observatoire-immigration.fr/"
+base_url = os.getenv("BASE_URL")
+if not base_url:
+    raise EnvironmentError("BASE_URL not found. Run with BASE_URL=https://mywebsite.fr/")
+
 EXCLUDED_PATHS = []
 
-discoverer = UrlDiscoverer(base_url=BASE_URL, excluded_paths=EXCLUDED_PATHS)
+discoverer = UrlDiscoverer(base_url, excluded_paths=EXCLUDED_PATHS)
 discoverer.discover_urls()
