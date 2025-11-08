b671872bccb4cfb0e401ff0d688a16ad0589fa61
Astrak <coffre.a@gmail.com>
2025-11-08 22:09:29 +0100

    Move folder names to env variables

--- src/scraper/download_articles/main.py
+++ src/scraper/download_articles/main.py
@@ -1,11 +1,17 @@
 from src.scraper.download_articles.article_dowloader import ArticleDownloader
+import os
 
-FOLDER = "./ojim-urls/"
+folder = os.getenv("FOLDER")
+if not folder:
+    raise EnvironmentError("FOLDER not found. Run with FOLDER='./my-folder/'")
 
-ARTICLE_SELECTOR = "#singleojimcontent" # "#contenu" pour Polémia
+article_selector = os.getenv("ARTICLE_SELECTOR")
+if not article_selector:
+    raise EnvironmentError("ARTICLE_SELECTOR not found. Run with ARTICLE_SELECTOR='#css-selector'")
 
-# Make sure to check the ArticleDownloader#scrape_article method and vet how metadata, author, date and title are selected.
+print("Downloading articles")
+print("Make sure to have checked the ArticleDownloader#scrape_article method and have vetted how metadata, author, date and title are selected.")
 
-discoverer = ArticleDownloader(folder=FOLDER, article_selector=ARTICLE_SELECTOR)
+discoverer = ArticleDownloader(folder, article_selector)
 discoverer.sync_urls_from_bucket()
 discoverer.scrape_articles()
--- src/scraper/vectorizer/main.py
+++ src/scraper/vectorizer/main.py
@@ -1,11 +1,15 @@
 from src.scraper.vectorizer.vectorizer import Vectorizer
 from env import fill_env
+import os
+
+folder = os.getenv("FOLDER")
+if not folder:
+    raise EnvironmentError("FOLDER not found. Run with FOLDER='./my-folder/'")
 
 fill_env()
 
-FOLDER = './ojim-urls/'
-CHECKPOINT_DIR = FOLDER + 'embeddings'
+CHECKPOINT_DIR = folder + 'embeddings'
 
-vectorizer = Vectorizer(folder=FOLDER, checkpoint_dir=CHECKPOINT_DIR)
+vectorizer = Vectorizer(folder, checkpoint_dir=CHECKPOINT_DIR)
 vectorizer.create_embeddings_with_checkpoint()
 vectorizer.create_chunked_faiss_system()
