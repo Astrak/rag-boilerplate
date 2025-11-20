694ab8f6bce456e213f0ff055aa5be0b8a37721d
Astrak <coffre.a@gmail.com>
2025-11-21 00:39:11 +0100

    Remove session object

--- src/apps/x/main.py
+++ src/apps/x/main.py
@@ -58,7 +58,6 @@ async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE):
                 print(answer.content)
                 result = answer.content.strip()
                 sessions[session_id] = {'context': [], 'discussion': [update.message.text, result]}
-            print(sessions[session_id])
             await update.message.reply_text(result, parse_mode="HTML", disable_web_page_preview=True)
         else:
             # implement flow: use graph or just chat?
