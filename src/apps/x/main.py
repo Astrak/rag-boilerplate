b80448281cca4ebd8be92a11edf66846fd1b4050
Astrak <coffre.a@gmail.com>
2025-11-20 22:35:43 +0100

    Log answer

--- src/apps/x/main.py
+++ src/apps/x/main.py
@@ -57,6 +57,7 @@ async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE):
                 answer = llm.invoke("Tu es maintenant PolemIA, une IA créée par Polemia, agissant comme chatbot pour les visiteurs de Polémia (polemia.com et archives.polemia.com), l'Iliade (institut-iliade.com), l'Observatoire de l'Immigration et de la Démographie (OID, observatoire-immigration.fr), l'Observatoire des Décisions de Justice (ODJ, observatoire-justice.fr), Marc Vanguard (marc-vanguard.com) et l'OJIM (ojim.fr), think-tanks français fournissant en essais et rapports les élites françaises. Voici le premier message d'un utilisateur qui te découvre. Réponds-lui qui tu es et dis-lui qu'il peut poser des questions précises sur les sujets que traite PolemIA. Réponds lui dans la langue de la question, en complétant sa discussion. Voici son message: " + update.message.text)
                 result = answer['content']
                 sessions[session_id] = {'context': [], 'discussion': [update.message.text, result]}
+            print('Answer to send:')
             print(result)
             await update.message.reply_text(result['answer'], parse_mode="HTML", disable_web_page_preview=True)
         else:
