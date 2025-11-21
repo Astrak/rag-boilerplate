2fe4290fa16643e1bdcb02f01821a5147754758b
Astrak <coffre.a@gmail.com>
2025-11-21 19:07:15 +0100

    Fix discussion

--- src/apps/x/main.py
+++ src/apps/x/main.py
@@ -89,13 +89,15 @@ async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE):
                 print('Run rag')
                 answer = graph.invoke(update.message.text)
                 result = answer['answer'].strip()
-                sessions[session_id] = {'context': answer['context'], 'discussion': sessions[session_id]['discussion'].extend([update.message.text, result])}
+                discussion.extend([update.message.text, result])
+                sessions[session_id] = {'context': answer['context'], 'discussion': discussion }
             else:
                 print('Run LLM without RAG')
                 question = prompt.invoke({"question": update.message.text, "discussion": "\n\n".join(discussion), "context": "\n\n".join(sessions[session_id]['context'])})
                 answer = llm.invoke(question)
                 result = answer.content.strip()
-                sessions[session_id] = {'context': sessions[session_id]['context'], 'discussion': sessions[session_id]['discussion'].extend([update.message.text, result])}
+                discussion.extend([update.message.text, result])
+                sessions[session_id] = {'context': sessions[session_id]['context'], 'discussion': discussion }
                 print(result)
             await update.message.reply_text(result, parse_mode="HTML", disable_web_page_preview=True)
     except Exception as e:
