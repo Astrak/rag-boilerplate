b4052f016650d8bba8fb086de5ffb29e907b18d8
Astrak <coffre.a@gmail.com>
2026-01-24 23:32:26 +0100

    Fix discarding sets of /pages/ when an existing one has no new article

--- src/scraper/discover_urls/url_discoverer.py
+++ src/scraper/discover_urls/url_discoverer.py
@@ -55,16 +55,17 @@ class UrlDiscoverer:
         print("Discovering URLs for " + self.folder)
         full_path = f"./knowledge-sources/{self.folder}"
         retained_urls = []
-        to_visit = set()
+        to_visit = []
         to_revisit = set()
         visited = set()
         ignored = IGNORE
         visit_but_dont_record = VISIT_BUT_DONT_RECORD
+        blacklisted = set()
         was_known = 0
         timer = datetime.utcnow()
         response = self.session.get(f"https://{self.folder}", timeout=10)
         self.base_url = response.url # Use main domain that the folder domain redirects to (with or without www)
-        to_visit.add(self.base_url)
+        to_visit.append(self.base_url)
         if os.path.exists(f'{full_path}/url-list.csv'):
             with open(f'{full_path}/url-list.csv', 'r', encoding='utf-8') as file:
                 csv_reader = csv.reader(file)
@@ -81,11 +82,9 @@ class UrlDiscoverer:
         if os.path.exists(f'{full_path}/blacklist.csv'):
             with open(f'{full_path}/blacklist.csv', 'r', encoding='utf-8') as file:
                 csv_reader = csv.reader(file)
-                counter = 0
                 for row in csv_reader:
-                    counter += 1
-                    visit_but_dont_record.add(row[0])
-                print(f"{counter} blacklisted URLs")
+                    blacklisted.add(row[0])
+                print(f"{len(blacklisted)} blacklisted URLs")
         if os.path.exists(f'{full_path}/ignore.csv'):
             with open(f'{full_path}/ignore.csv', 'r', encoding='utf-8') as file:
                 csv_reader = csv.reader(file)
@@ -105,9 +104,13 @@ class UrlDiscoverer:
                 # TODO if there was a redirect remove this URL and if it also was in another set or array, modify it as well.
                 visited.add(current_url)
                 soup = BeautifulSoup(response.content, 'html.parser')
-                page_links = set()
-                links = [cast(str, href.get('href')) for href in soup.select("a[href]")]
+                links = {cast(str, href.get('href')) for href in soup.select("a[href]")}
+                if "#" in links:
+                    links.remove("#")
+                new_article_found = False
                 for href in links:
+                    if "mailto:" in href:
+                        continue
                     href = self._ensure_url_is_absolute(href)
                     href = href.split('#')[0] # Discard hashes
                     href = href.split('?')[0] # Discard queries
@@ -116,26 +119,32 @@ class UrlDiscoverer:
                     href = "://".join(split)
                     is_ignored = any(sub in href for sub in ignored)
                     dont_visit_but_record = any(sub in href for sub in DONT_VISIT_BUT_RECORD)
-                    dont_record_but_visit = any(sub in href for sub in visit_but_dont_record)
+                    dont_record_but_visit = any(sub in href for sub in visit_but_dont_record) or href in blacklisted
                     if self._is_same_domain(href) and not is_ignored and href not in visited:
-                        page_links.add(href)
                         if href not in to_visit and not dont_visit_but_record:
-                            to_visit.add(href)
+                            to_visit.append(href)
                         if href not in retained_urls and not dont_record_but_visit:
                             retained_urls.append(href)
-                # if the only remaining links in a page are further pages of search results, 
-                # typically of the form "page/5" in the URL, 
-                # don't look further into similarly prefixed pages.
-                # (supposes that no line of older articles in an existing url-list.csv are removed)
-                # TODO: check if a number follows with regexp otherwise it may also just be a url like /page/my-article
-                if re.search("/page/", current_url) and len(page_links) and all("/page/" in url for url in page_links):
+                            new_article_found = True
+                if "/page/" in current_url and not new_article_found:
                     prefix = current_url.split("/page/")[0] + "/page/"
-                    # TODO This is wrong because when a first page shows links to page 2, 3, and then 10,
-                    # Then the next pages to open are 2, 3 and 10: pages 4 and following will be visited after 10
-                    # If page 10 is already visited but not pages between 4 and 10, new links may not be discovered.
-                    # => Sort the urls and always visit the first in alphanumerical order, and only delete the similar
-                    # ones that are next 
-                    to_visit = {url for url in to_visit if not url.startswith(prefix)}
+                    pattern = re.compile(re.escape(prefix) + r'(\d+)(?:/|$)')
+                    to_visit.sort(
+                        reverse=True,
+                        key=lambda s: int(m.group(1)) if (m := pattern.match(s)) else float('inf')
+                    )
+                    page_number = int(re.search(r'/page/(\d+)(?:/|$)', current_url).group(1))
+                    i = 0
+                    while i < len(to_visit):
+                        item = to_visit[i]
+                        if "/page/" in item and item.startswith(prefix):
+                            page = int(re.search(r'/page/(\d+)(?:/|$)', item).group(1))
+                            if page < page_number: 
+                                i += 1
+                            else:
+                                del to_visit[i]
+                        else:
+                            i += 1
                 sys.stdout.write(f"\033[F\033[F\033[F\033[F\033[F\033[F")
                 sys.stdout.write(f"\r\033[KVisiting: {current_url}\n") 
                 sys.stdout.write(f"\r\033[KVisited: {len(visited) - was_known}\n") 
