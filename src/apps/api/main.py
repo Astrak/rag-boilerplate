eb0f2d1167307934ddaaced87170575be4da1a4c
Astrak <coffre.a@gmail.com>
2026-01-28 16:22:33 +0100

    Don't print page

--- src/apps/api/main.py
+++ src/apps/api/main.py
@@ -29,7 +29,6 @@ try:
         folders = []
         paginator = s3_client.get_paginator("list_objects_v2")
         for page in paginator.paginate(Bucket="rag-faiss-index-bucket"):
-            print(page)
             for cp in page.get("CommonPrefixes", []):
                 full_prefix = cp["Prefix"]
                 subfolder = full_prefix[len("knowledge-sources/"):]
