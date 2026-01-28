d76c4649705e25d1e3c75894ecc7c9fe2be4342c
Astrak <coffre.a@gmail.com>
2026-01-28 11:37:07 +0100

    Update Vectorizer to new knowledge-sources structure

--- src/scraper/vectorizer/main.py
+++ src/scraper/vectorizer/main.py
@@ -2,18 +2,25 @@ from src.scraper.vectorizer.vectorizer import Vectorizer
 from src.scraper.vectorizer.env import fill_env
 import os
 
-folder = os.getenv("FOLDER")
-if not folder:
-    raise EnvironmentError("FOLDER not found. Run with FOLDER='./my-folder/'")
+sources = os.getenv("SOURCES")
+if not sources:
+    print("No SOURCES variable specified, vectorizing all sources in the knowledge-sources folder:")
+    sources = os.listdir("./knowledge-sources/")
+else:
+    sources = sources.split(',')
+print("Vectorize text from sources: ", sources)
 
 fill_env()
 
-CHECKPOINT_DIR = folder + 'embeddings'
-
 def main():
-    vectorizer = Vectorizer(folder, checkpoint_dir=CHECKPOINT_DIR)
-    vectorizer.create_embeddings_with_checkpoint()
-    vectorizer.create_chunked_faiss_system()
+    for source in sources:
+        try:
+            vectorizer = Vectorizer(source)
+            vectorizer.create_embeddings_with_checkpoint()
+            vectorizer.create_chunked_faiss_system()
+            vectorizer.sync_embeddings_to_bucket()
+        except Exception as e:
+            print(f"Failed to vectorize {source}: ", e)
 
 if __name__ == "__main__":
     main()
