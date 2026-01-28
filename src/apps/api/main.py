b0042a44557009daffb30489d206f9aed02fd4b2
Astrak <coffre.a@gmail.com>
2026-01-28 17:01:24 +0100

    Attempt fix 5

--- src/apps/api/main.py
+++ src/apps/api/main.py
@@ -32,14 +32,14 @@ try:
             for cp in page.get("Contents", []):
                 file = cp["Key"]
                 files.append(file)
-        # for file in files:
-        print(f"Downloading {files[0]} into {os.getcwd()}/knowledge-sources/{files[0]}")
-        os.makedirs(os.path.dirname(f"{os.getcwd()}/knowledge-sources/{files[0]}"), exist_ok=True)
-        s3.download_file(
-            Bucket="rag-faiss-index-bucket", 
-            Filename=f"knowledge-sources/{files[0]}", 
-            Key=files[0]
-        )
+        for file in files:
+            print(f"Downloading {files[0]} into {os.getcwd()}/knowledge-sources/{files[0]}...")
+            os.makedirs(os.path.dirname(f"{os.getcwd()}/knowledge-sources/{files[0]}"), exist_ok=True)
+            s3.download_file(
+                Bucket="rag-faiss-index-bucket", 
+                Filename=f"knowledge-sources/{files[0]}", 
+                Key=files[0]
+            )
 except Exception as e:
     raise ValueError('Didnt understand what to do with knowledge sources', e)
 
