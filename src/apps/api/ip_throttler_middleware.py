c5fe78bf541cf9cbbd112c8e53dbb34029185752
Astrak <coffre.a@gmail.com>
2026-03-03 23:07:15 +0100

    Throttle per min and day

--- src/apps/api/ip_throttler_middleware.py
+++ src/apps/api/ip_throttler_middleware.py
@@ -3,7 +3,9 @@ from fastapi import Request
 from fastapi.responses import JSONResponse
 
 IP_THROTTLER = {}
-COOLDOWN = timedelta(seconds=3)
+MIN_WAITING_TIME = timedelta(seconds=3)
+MAX_REQ_PER_MIN = 3
+MAX_REQ_PER_DAY = 10
 
 class IPThrottleMiddleware:
     def __init__(self, app):
@@ -23,15 +25,33 @@ class IPThrottleMiddleware:
             return
 
         if ip in IP_THROTTLER:
-            delay = now - IP_THROTTLER[ip]
-            if delay < COOLDOWN:
+            last_request = IP_THROTTLER[ip][-1]
+            delay = now - last_request
+            requests_in_last_minute = len([date for date in IP_THROTTLER[ip] if (now - date) < datetime(minute=1)])
+            requests_in_last_day = len([date for date in IP_THROTTLER[ip] if (now - date) < datetime(day=1)])
+            if delay < MIN_WAITING_TIME:
                 print(f"\033[91mTHROTTLED {ip} — {str(delay)} elapsed since last request\033[0m")
                 response = JSONResponse(
                     status_code=429,
-                    content={"error": "Rate limit reached"}
+                    content={"error": "Ddos protection reached"}
                 )
                 await response(scope, receive, send)
-                return
-
-        IP_THROTTLER[ip] = now
+            elif requests_in_last_minute > MAX_REQ_PER_MIN:
+                print(f"\033[91mTHROTTLED {ip} — {requests_in_last_minute} requests in last minute\033[0m")
+                response = JSONResponse(
+                    status_code=429,
+                    content={"error": "Max requests per minute reached"}
+                )
+                await response(scope, receive, send)
+            elif requests_in_last_day > MAX_REQ_PER_DAY:
+                print(f"\033[91mTHROTTLED {ip} — {requests_in_last_day} requests in last day\033[0m")
+                response = JSONResponse(
+                    status_code=429,
+                    content={"error": "Max requests per day reached"}
+                )
+                await response(scope, receive, send)
+            IP_THROTTLER[ip].append(now)
+        else:
+            IP_THROTTLER[ip] = [now]
+            
         await self.app(scope, receive, send)
